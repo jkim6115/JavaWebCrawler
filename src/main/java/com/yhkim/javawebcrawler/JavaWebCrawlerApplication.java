b138d1be4d38402c9f6564865fa1ceb60b98183c
YOHAN <jkim6115@gmail.com>
2023-02-28 14:45:54 +0900

    2023-02-28 init git

---
+++ src/main/java/com/yhkim/javawebcrawler/JavaWebCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.yhkim.javawebcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhkim/javawebcrawler/JavaWebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhkim.javawebcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
